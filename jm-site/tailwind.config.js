ecb68deee71f59328d59e59e8f3e80fd1e2bd376
Mcdaid101 <matthewmcdaid1@hotmail.com>
2024-07-03 19:03:51 +0100

    Initial commit

---
+++ jm-site/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './views/**/*.ejs',
+    './public/**/*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
